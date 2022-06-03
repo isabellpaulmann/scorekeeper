1b269f0f68a70ab1ee96529bcd749c71e3e53791
Isabell Paulmann <isabell.paulmann@hotmail.de>
2022-06-03 15:53:29 +0200

    add functions

--- src/Player.js
+++ src/Player.js
@@ -1,13 +1,17 @@
 import "./Player.css";
 
-export default function Player({ playerDetails }) {
+export default function Player({ name, score, onAddScore, onReduceScore }) {
   return (
-    <div className="Name__score">
-      {playerDetails.name}
-      <div className="Name__buttons">
-        <button className="button1">-</button>
-        {playerDetails.score}
-        <button className="button2">-</button>
+    <div className="Player__score">
+      {name}
+      <div className="Player__buttons">
+        <button onClick={onReduceScore} className="button1">
+          -
+        </button>
+        <p className="Player__score--number">{score}</p>
+        <button onClick={onAddScore} className="button2">
+          +
+        </button>
       </div>
     </div>
   );
